41da24a48c96c019023e76c59b3d2e67d2347081
Turkei <graham-barnes@hotmail.co.uk>
2023-12-05 16:57:13 +0000

    Read a book and count the words

--- main.py
+++ main.py
@@ -1,1 +1,9 @@
-print("It worked")
+with open("books/frankenstein.txt") as f:
+
+    # Import the book
+    file_contents = f.read()
+    print(file_contents)
+
+    # Calculate word count
+    words = file_contents.split()
+    print(f"\nThere are {len(words)} words in this book.")
