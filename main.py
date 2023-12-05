c06c53eda282fef48d886b4076ac4474ae88fd5e
Turkei <graham-barnes@hotmail.co.uk>
2023-12-05 17:06:55 +0000

    Added count of individual letters

--- main.py
+++ main.py
@@ -2,8 +2,19 @@ with open("books/frankenstein.txt") as f:
 
     # Import the book
     file_contents = f.read()
-    print(file_contents)
-
+    
     # Calculate word count
     words = file_contents.split()
     print(f"\nThere are {len(words)} words in this book.")
+
+    # Calculate letter count
+    letter_count = {}
+
+    for letter in file_contents.lower():
+        if (letter in letter_count):
+            letter_count[letter] += 1
+        else:
+            letter_count[letter] = 1
+
+    print()
+    print(letter_count)
